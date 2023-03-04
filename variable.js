c693965225e77439ed7be339ad12ec501dd9ca3a
macbook15 <macbook15@MacBook-Pro-macbook15.local>
2023-03-04 13:45:18 +0700

    feat: add basic-js

---
+++ variable.js
@@ -0,0 +1,13 @@
+// '' string petik2 / petik 1 (kata, huruf)
+// 7 contoh ngnggone angka langsung integer (angka)
+// {} objek (data kompleks)
+// bolean true / false
+// undifined (buat variable tapi renek nilainya)
+
+// varible 1.const(data pasti) 2.let 3.var (iso dirubah)
+const nama = 'gilang'
+console.log(nama)
+var umur = 18
+var umur = 19
+
+console.log(umur)
